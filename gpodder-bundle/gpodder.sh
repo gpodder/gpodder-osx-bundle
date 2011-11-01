7e9d0724c285d82dc5d64e3ae85c96ae9e960834
Eric Le Lay <elelay@macports.org>
2011-11-01 17:08:25 +0100

    initial import

---
+++ gpodder-bundle/gpodder.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+name=`basename "$0"`
+tmp="$0"
+tmp=`dirname "$tmp"`
+tmp=`dirname "$tmp"`
+bundle=`dirname "$tmp"`
+bundle_contents="$bundle"/Contents
+bundle_res="$bundle_contents"/Resources
+bundle_lib="$bundle_res"/lib
+bundle_bin="$bundle_res"/bin
+bundle_data="$bundle_res"/share
+bundle_etc="$bundle_res"/etc
+
+export DYLD_LIBRARY_PATH="$bundle_lib"
+export XDG_CONFIG_DIRS="$bundle_etc"/xdg
+export XDG_DATA_DIRS="$bundle_data"
+export GTK_DATA_PREFIX="$bundle_res"
+export GTK_EXE_PREFIX="$bundle_res"
+export GTK_PATH="$bundle_res"
+
+export GTK2_RC_FILES="$bundle_etc/gtk-2.0/gtkrc"
+export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
+export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
+export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
+
+#Set $PYTHON to point inside the bundle
+export PYTHON="$bundle_contents/MacOS/python"
+#Add the bundle's python modules
+PYTHONPATH="$bundle_lib/python2.7:$PYTHONPATH"
+PYTHONPATH="$bundle_lib/python2.7/site-packages:$PYTHONPATH"
+#Add our program's modules to $PYTHONPATH. 
+PYTHONPATH="$bundle_lib/python2.7/gtk-2.0:$PYTHONPATH"
+export PYTHONPATH
+echo "PYTHONPATH=$PYTHONPATH"
+PYTHONHOME="$bundle_res"
+export PYTHONHOME
+# We need a UTF-8 locale.
+lang=`defaults read .GlobalPreferences AppleLocale 2>/dev/null`
+if test "$?" != "0"; then
+  lang=`defaults read .GlobalPreferences AppleCollationOrder 2>/dev/null | sed 's/_.*//'`
+fi
+if test "$?" == "0"; then
+    export LANG="`grep \"\`echo $lang\`_\" /usr/share/locale/locale.alias | \
+  tail -n1 | sed 's/\./ /' | awk '{print $2}'`.UTF-8"
+fi
+
+if test -f "$bundle_lib/charset.alias"; then
+    export CHARSETALIASDIR="$bundle_lib"
+fi
+
+# Extra arguments can be added in environment.sh.
+EXTRA_ARGS=
+if test -f "$bundle_res/environment.sh"; then
+  source "$bundle_res/environment.sh"
+fi
+
+# Strip out the argument added by the OS.
+if /bin/expr "x$1" : "x-psn_.*" > /dev/null; then
+    shift 1
+fi
+export LANG="fr_FR.UTF-8"
+
+# use launchctl to load the session dbus if not already started by the session
+if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+    if test ! -f $bundle_res/var/lib/dbus/machine-id ; then
+        echo generating dbus machine id
+        mkdir -p $bundle_res/var/lib/dbus
+        $bundle_bin/dbus-uuidgen --ensure=$bundle_res/var/lib/dbus/machine-id
+    fi
+    plist="$bundle_res"/Library/LaunchAgents/org.freedesktop.dbus-session.plist
+    daemon_path="$bundle_res"/bin/dbus-daemon
+    if ! grep -q $daemon_path "$plist" ; then
+        echo rewritting plist
+        /usr/bin/sed "s,@BUNDLE_RES@,$bundle_res," $plist.in > $plist
+    fi
+    echo launching session dbus
+    launchctl load $bundle_res/Library/LaunchAgents/org.freedesktop.dbus-session.plist
+fi
+
+#Note that we're calling $PYTHON here to override the version in
+#pygtk-demo's shebang.
+$EXEC $PYTHON "$bundle_contents/Resources/bin/gpodder" "-v"
+
+# unload the session dbus on exit 
+if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+    echo unlaunch session dbus
+    launchctl unload $bundle_res/Library/LaunchAgents/org.freedesktop.dbus-session.plist
+fi
---
+++ gpodder-bundle/postproc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/distutils
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/idlelib
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/lib-tk
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/lib2to3
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/pydoc-data
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/test
+rm -Rf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/unittest
+rm -Rvf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/*/test
+rm -f ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/config/libpython2.7.a
+find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyc' -delete
+find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyo' -delete
