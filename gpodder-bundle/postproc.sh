35fe360694689ba79a0ddd45f380f0225b90c9e2
Eric Le Lay <elelay@macports.org>
2011-11-08 21:16:56 +0100

    switch to gPodder 3.3.0

--- gpodder-bundle/postproc.sh
+++ gpodder-bundle/postproc.sh
@@ -11,3 +11,4 @@ rm -Rvf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/*/test
 rm -f ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/config/libpython2.7.a
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyc' -delete
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyo' -delete
+rm ~/Desktop/gPodder.app/Contents/MacOS/gPodder-bin
