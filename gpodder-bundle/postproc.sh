4f50a01686c6aab7a431765c2664607fb5c48846
Eric Le Lay <elelay@macports.org>
2012-01-12 18:10:24 +0100

    Command-XX shortcuts in gPodder menus

--- gpodder-bundle/postproc.sh
+++ gpodder-bundle/postproc.sh
@@ -7,3 +7,6 @@ rm -f ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/config/libpython2.7
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyc' -delete
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyo' -delete
 rm ~/Desktop/gPodder.app/Contents/MacOS/gPodder-bin
+
+# Command-XX shortcuts in gPodder menus 
+/usr/bin/sed -i "" 's/GDK_CONTROL_MASK/GDK_META_MASK/' ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/desktop/gpodder.ui
