985e3a2c29b75b1dde31cf38d92b7b733b95196b
Eric Le Lay <elelay@macports.org>
2012-03-27 19:51:27 +0200

    ui moved : share/gpodder/ui/{desktop,gtk}

--- gpodder-bundle/postproc.sh
+++ gpodder-bundle/postproc.sh
@@ -6,10 +6,10 @@ rm -Rvf ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/*/test
 rm -f ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7/config/libpython2.7.a
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyc' -delete
 find ~/Desktop/gPodder.app/Contents/Resources/lib/python2.7 -name '*.pyo' -delete
-rm ~/Desktop/gPodder.app/Contents/MacOS/gPodder-bin
+rm -f ~/Desktop/gPodder.app/Contents/MacOS/gPodder-bin
 rm -Rf ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/qml
 
 # Command-XX shortcuts in gPodder menus 
-/usr/bin/xsltproc -o gpodder.ui.tmp adjust-modifiers.xsl ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/desktop/gpodder.ui
-mv gpodder.ui.tmp ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/desktop/gpodder.ui
+/usr/bin/xsltproc -o gpodder.ui.tmp adjust-modifiers.xsl ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/gtk/gpodder.ui
+mv gpodder.ui.tmp ~/Desktop/gPodder.app/Contents/Resources/share/gpodder/ui/gtk/gpodder.ui
 
