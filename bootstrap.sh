be0bb8635838f50dee401cd510c2593bdbaee876
Eugene Nikolsky <eunikolsky@gmail.com>
2016-04-24 21:05:59 -0700

    bootstrap: allow bootstrapping again

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+set -e
+
 source env.sh
+
+# to allow bootstrapping again, try to delete everything first
+rm -Rf "_jhbuild"
+rm -Rf "_bundler"
+rm -Rf "$HOME/.local"
+rm -f "$HOME/.jhbuildrc"
+rm -f "$HOME/.jhbuildrc-custom"
 
 mkdir -p "$HOME"
 git clone git://git.gnome.org/jhbuild _jhbuild
