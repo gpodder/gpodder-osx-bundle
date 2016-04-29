272cab2827068ff73387f777d7c3c71d73544acf
Eugene Nikolsky <eunikolsky@gmail.com>
2016-04-28 22:30:28 -0700

    bootstrap.sh: checkout specific `jhbuild` commit
    This makes sure we're using a stable version. Based on the official
    jhbuild setup script:
    https://git.gnome.org/browse/gtk-osx/plain/gtk-osx-build-setup.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,12 @@ rm -Rf "$HOME/.local"
 rm -f "$HOME/.jhbuildrc"
 rm -f "$HOME/.jhbuildrc-custom"
 
+# https://git.gnome.org/browse/gtk-osx/tree/jhbuild-revision
+JHBUILD_REVISION="7c8d34736c3804"
+
 mkdir -p "$HOME"
 git clone git://git.gnome.org/jhbuild _jhbuild
-(cd _jhbuild && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
+(cd _jhbuild && git checkout "$JHBUILD_REVISION" && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
 cp misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
 cp misc/jhbuildrc-custom "$HOME/.jhbuildrc-custom"
 git clone git://git.gnome.org/gtk-mac-bundler _bundler
