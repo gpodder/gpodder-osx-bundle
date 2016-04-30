fa38174b485f4346a79f64d5149616400e7e37f7
Eugene Nikolsky <eunikolsky@gmail.com>
2016-04-29 22:31:29 -0700

    env.sh: reset $PATH to defaults
    This ensures only system binaries are used in the build process.

--- env.sh
+++ env.sh
@@ -3,6 +3,10 @@
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$DIR"
 
+# reset `$PATH` to defaults to make sure there are no non-standard
+# binaries available, e.g. from homebrew
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
+
 export HOME="$DIR/_home"
 export PATH="$PATH:$HOME/.local/bin:$HOME/jhbuild_prefix/bin"
 export QL_OSXBUNDLE_MODULESETS_DIR="$DIR/modulesets"
