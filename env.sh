b8dc9c92f8c8c3fa5c316ad36f4878d8ea0b6748
Eugene Nikolsky <eunikolsky@gmail.com>
2016-04-29 22:32:37 -0700

    env.sh: short-circuit java binaries to `false`
    This avoids an annoying dialog on OSX in case java is not installed in
    the system (which is the default).

--- env.sh
+++ env.sh
@@ -7,6 +7,12 @@ cd "$DIR"
 # binaries available, e.g. from homebrew
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin"
 
+# java is unnecessary for gpodder; but some packages check for its
+# presence at configuration step; on OSX, if java is not installed,
+# an annoying dialog appears => to avoid that, we alias java-related
+# commands to return an error exit code
+export PATH="$DIR/fake_java:$PATH"
+
 export HOME="$DIR/_home"
 export PATH="$PATH:$HOME/.local/bin:$HOME/jhbuild_prefix/bin"
 export QL_OSXBUNDLE_MODULESETS_DIR="$DIR/modulesets"
