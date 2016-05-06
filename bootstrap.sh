693b805383a4c26585e5f30e6c7acdaa8399b507
Eugene Nikolsky <eunikolsky@gmail.com>
2016-05-05 20:24:28 -0700

    bootstrap.sh: link `jhbuildrc` files instead of copying
    IMHO, in most cases, if you modify the files in the custom `$HOME`
    location, you want the changes to be reflected in the repository for
    easier tracking and committing.

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,7 @@ JHBUILD_REVISION="7c8d34736c3804"
 mkdir -p "$HOME"
 git clone git://git.gnome.org/jhbuild _jhbuild
 (cd _jhbuild && git checkout "$JHBUILD_REVISION" && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
-cp misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
-cp misc/jhbuildrc-custom "$HOME/.jhbuildrc-custom"
+ln misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
+ln misc/jhbuildrc-custom "$HOME/.jhbuildrc-custom"
 git clone git://git.gnome.org/gtk-mac-bundler _bundler
 (cd _bundler && make install)
