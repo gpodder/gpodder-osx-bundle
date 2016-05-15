a98f637f1f4f7d81a86b8e9f0cff18fe4945d9ef
Eric Le Lay <elelay@macports.org>
2016-05-15 13:19:45 +0200

    use gdk-pixbuf loaders in lib
    they are generated by gtk-mac-bundler so postprocessed
    to have bundle-relative paths

--- misc/bundle/launcher.sh
+++ misc/bundle/launcher.sh
@@ -18,7 +18,7 @@ export GTK_PATH="$bundle_res"
 
 export GTK2_RC_FILES="$bundle_etc/gtk-2.0/gtkrc"
 export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
-export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
+export GDK_PIXBUF_MODULE_FILE="$bundle_lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
 
 # Pango wants these 2 otherwise it displays square boxes instead of letters
 export PANGO_SYSCONFDIR="$bundle_etc"
