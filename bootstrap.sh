37f4fa2ddebe9af513970df9048ed7af76f4c9c1
Eric Le Lay <elelay@macports.org>
2016-06-05 09:23:05 +0200

    python2.6 compatible jhbuild

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,10 @@ JHBUILD_REVISION="7c8d34736c3804"
 
 mkdir -p "$HOME"
 git clone git://git.gnome.org/jhbuild _jhbuild
-(cd _jhbuild && git checkout "$JHBUILD_REVISION" && ./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
+# https://bugzilla.gnome.org/show_bug.cgi?id=766444
+(cd _jhbuild && git checkout "$JHBUILD_REVISION" && \
+	git am ../modulesets/patches/01_27891.patch ../modulesets/patches/02_327933.patch && \
+	./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
 ln misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
 ln misc/jhbuildrc-custom "$HOME/.jhbuildrc-custom"
 git clone git://git.gnome.org/gtk-mac-bundler _bundler
