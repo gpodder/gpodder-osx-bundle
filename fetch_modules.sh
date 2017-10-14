0fd6b7e4a847367de047c1df23a58f00fd65d66b
Eric Le Lay <elelay@macports.org>
2017-10-14 17:17:41 +0200

    update modulesets to latest upstream
    switch to github for mutagen and mygpoclient
    save upstream reference
    remove unused patches

--- fetch_modules.sh
+++ fetch_modules.sh
@@ -5,4 +5,5 @@ source env.sh
 git clone git://git.gnome.org/gtk-osx _gtk-osx
 cp -R _gtk-osx/modulesets-stable/. modulesets/
 cp _gtk-osx/jhbuildrc-gtk-osx misc/gtk-osx-jhbuildrc
+(cd _gtk-osx/ && git log -1) > modulesets/upstream-ref
 rm -Rf _gtk-osx
