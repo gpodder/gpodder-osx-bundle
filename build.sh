3b155a45ba0873ad3b1101b092c19ae7df1b70fb
Eric Le Lay <elelay@macports.org>
2018-01-01 21:39:58 +0100

    successful build for macOS 10.9

--- build.sh
+++ build.sh
@@ -7,5 +7,8 @@ source env.sh
 jhbuild bootstrap
 jhbuild build openssl
 jhbuild build python3
+# itstool will not work with python3: libxml2 doesn't compile with python3
+jhbuild build itstool
+export PYTHON=$HOME/jhbuild_prefix/bin/python3
 jhbuild build meta-gtk-osx-bootstrap
 jhbuild build gpodder
