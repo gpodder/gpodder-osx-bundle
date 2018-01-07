cff557c05f2dd9d40f984418f9f2f5b300e70d9d
Eric Le Lay <elelay@macports.org>
2018-01-07 21:14:04 +0100

    debug home?

--- travis_bootstrap.sh
+++ travis_bootstrap.sh
@@ -12,6 +12,7 @@ cat "_home/.ssh/known_hosts"
 openssl aes-256-cbc -K $encrypted_66daf52526ba_key -iv $encrypted_66daf52526ba_iv -in misc/travis/gpodderbuild.enc -out ../gpodderbuild -d
 chmod go-wrx ../gpodderbuild
 
+echo "HOME is $HOME!"
 # upload data
 rsync -e "ssh -p$RSYNC_PORT -i ../gpodderbuild -o StrictHostKeyChecking=no" -avrz --exclude "$HOME/jhbuild_checkoutroot" \
 	_jhbuild \
