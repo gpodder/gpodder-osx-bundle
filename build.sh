90a127935baa83ba3ebc5cbf661b1315cbe70be5
Eric Le Lay <elelay@macports.org>
2018-02-03 21:19:01 +0100

    remove travis support

--- build.sh
+++ build.sh
@@ -4,20 +4,11 @@ set -e
 
 source env.sh
 
-if [ "$1" == "--python3" ]; then
-	export PYTHON=$HOME/jhbuild_prefix/bin/python3
-	shift
-fi
-if [ -z "$1" ]; then
-	jhbuild bootstrap
-	jhbuild build openssl
-	jhbuild build python3
-	# itstool will not work with python3: libxml2 doesn't compile with python3
-	jhbuild build itstool
-	export PYTHON=$HOME/jhbuild_prefix/bin/python3
-	jhbuild build meta-gtk-osx-bootstrap
-	jhbuild build gpodder
-else
-	echo "jhbuild $@"
-	jhbuild "$@"
-fi
+jhbuild bootstrap
+jhbuild build openssl
+jhbuild build python3
+# itstool will not work with python3: libxml2 doesn't compile with python3
+jhbuild build itstool
+export PYTHON=$HOME/jhbuild_prefix/bin/python3
+jhbuild build meta-gtk-osx-bootstrap
+jhbuild build gpodder
