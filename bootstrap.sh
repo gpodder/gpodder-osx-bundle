2112011f4285a93b16cdc4da53d3befee51ad9c5
Eric Le Lay <elelay@macports.org>
2018-09-05 21:38:02 +0200

    gnome git moved to gitlab.gnome.org

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,12 +15,12 @@ rm -f "$HOME/.jhbuildrc-custom"
 JHBUILD_REVISION="7c8d34736c3804"
 
 mkdir -p "$HOME"
-git clone git://git.gnome.org/jhbuild _jhbuild
+git clone  https://gitlab.gnome.org/GNOME/jhbuild.git _jhbuild
 # https://bugzilla.gnome.org/show_bug.cgi?id=766444
 (cd _jhbuild && git checkout "$JHBUILD_REVISION" && \
 	git am ../modulesets/patches/01_27891.patch ../modulesets/patches/02_327933.patch && \
 	./autogen.sh && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
 ln misc/gtk-osx-jhbuildrc "$HOME/.jhbuildrc"
 ln misc/jhbuildrc-custom "$HOME/.jhbuildrc-custom"
-git clone git://git.gnome.org/gtk-mac-bundler _bundler
+git clone https://gitlab.gnome.org/GNOME/gtk-mac-bundler.git _bundler
 (cd _bundler && make install)
