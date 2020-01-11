a64d33c31b754f982c041ee5b0d772304597163f
Eric Le Lay <elelay@macports.org>
2020-01-11 15:55:11 +0100

    no need for env.sh for fetch_modules.sh

--- fetch_modules.sh
+++ fetch_modules.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-source env.sh
-
 git clone https://gitlab.gnome.org/GNOME/gtk-osx.git _gtk-osx-modules
 rsync -vrb --delete --exclude gpodder.modules --exclude patches/gpodder* _gtk-osx-modules/modulesets-stable/ modulesets/
 (cd _gtk-osx-modules/ && git log -1) > modulesets/upstream-ref
