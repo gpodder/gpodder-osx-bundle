78392f8380d1e9a504a88125a8eb5fa3eb70aab4
Eric Le Lay <elelay@macports.org>
2022-07-27 14:48:47 +0200

    remove inexistant python3x.zip from pythonpath

--- misc/bundle/launcher.c
+++ misc/bundle/launcher.c
@@ -91,8 +91,6 @@ set_python_path(void)
     CFStringRef str = make_filesystem_string(bundle_url);
     CFRelease(bundle_url);
     mstr = CFStringCreateMutableCopy(NULL, 5 * PATH_MAX, str);
-    CFStringAppendCString(mstr, "/lib/python36.zip:", kCFStringEncodingUTF8);
-    CFStringAppend(mstr, str);
     CFStringAppendCString(mstr, "/lib/python3.9:",
         kCFStringEncodingUTF8);
     CFStringAppend(mstr, str);
