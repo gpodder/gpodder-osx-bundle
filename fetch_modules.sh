8cb50a02ab21736f403ab17a537323e2dd07fb02
auouymous <au@qzx.com>
2023-10-03 16:07:05 -0600

    Disable brotli when building freetype.

--- fetch_modules.sh
+++ fetch_modules.sh
@@ -5,3 +5,6 @@ rsync -vrb --delete --exclude gpodder.modules --exclude patches/gpodder* _gtk-os
 rsync -vrb --delete _gtk-osx-modules/patches modulesets/
 (cd _gtk-osx-modules/ && git log -1) > modulesets/upstream-ref
 rm -Rf _gtk-osx-modules
+
+# Disable check for brotli when building freetype
+(cd modulesets && patch -p1 < ../github-brotli.patch)
