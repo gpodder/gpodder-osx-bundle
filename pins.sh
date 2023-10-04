4670244a74fca29429db850159b3f427db25fa8f
auouymous <au@qzx.com>
2023-10-03 19:05:48 -0600

    Add URL comments to get latest commits for pinning.

--- pins.sh
+++ pins.sh
@@ -1,5 +1,7 @@
+# https://gitlab.gnome.org/GNOME/gtk-osx/-/commits/master
 # use latest commit (2023-08-20)
 PIN_GTK_OSX_COMMIT='a27e8ff6'
 
+# https://gitlab.gnome.org/GNOME/gtk-mac-bundler/-/commits/master
 # use latest commit (2023-08-17)
 PIN_GTK_MAC_BUNDLER_COMMIT='0a95b2d5'
