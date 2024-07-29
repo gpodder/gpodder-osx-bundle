14e462aee1442d81657bd9ebd02dbc7bceff657a
auouymous <au@qzx.com>
2024-07-29 00:18:06 -0600

    Update modulesets.

--- pins.sh
+++ pins.sh
@@ -1,7 +1,7 @@
 # https://gitlab.gnome.org/GNOME/gtk-osx/-/commits/master
-# use latest commit (2023-08-20)
-PIN_GTK_OSX_COMMIT='a27e8ff6'
+# use latest commit (2024-07-24)
+PIN_GTK_OSX_COMMIT='b0864c09'
 
 # https://gitlab.gnome.org/GNOME/gtk-mac-bundler/-/commits/master
-# use latest commit (2023-08-17)
-PIN_GTK_MAC_BUNDLER_COMMIT='0a95b2d5'
+# use latest commit (2024-05-19)
+PIN_GTK_MAC_BUNDLER_COMMIT='90443a1d'
